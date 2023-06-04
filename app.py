76bbd953671036ea4dcd7ba522826ca51da959e5
jw0622 <81131906+jw0622@users.noreply.github.com>
2023-06-04 23:50:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, request, jsonify
 import openai
 
 app = Flask(__name__)
@@ -7,22 +8,24 @@ openai.api_key = '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'
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    message = request.json['message']
+    if request.method == 'POST':
+        message = request.form['message']
 
-    response = openai.Completion.create(
-        engine='davinci',
-        prompt=message,
-        max_tokens=50,
-        n=1,
-        stop=None,
-        temperature=0.7
-    )
+        response = openai.Completion.create(
+            engine='davinci',
+            prompt=message,
+            max_tokens=50,
+            n=1,
+            stop=None,
+            temperature=0.7
+        )
 
-    return jsonify({'message': response.choices[0].text.strip()})
+        return jsonify({'message': response.choices[0].text.strip()})
 
 if __name__ == '__main__':
     app.run()
+
